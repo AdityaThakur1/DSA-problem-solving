e9965e7409007ced84dc1b362176850d80e907cd
Aditya Thakur <adityat354@gmail.com>
2023-05-18 02:34:42 -0400

    Time: 229 ms (5.04%), Space: 17.1 MB (34.14%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,22 +1,17 @@
 class Solution:
-    def bin_search(self, nums: List[int], target: int):
-        left, right = 0, len(nums)-1      
-        while(left <= right):
-            mid = (left + right)//2
-            if(target == nums[mid]):
-                return mid
-            elif (target < nums[mid]):
-                right = mid - 1
-            else :
-                left = mid + 1
-        return None
-    
+   
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         for i in range(len(numbers)):
             t1 = target - numbers[i]
-            t2 = self.bin_search(numbers[i+1:],t1)
             
-            if(t2 is not None):
-                return(i+1,i+t2+2)
-            
+            left, right = i+1, len(numbers)-1      
+            while(left <= right):
+                mid = (left + right)//2
+                if(t1 == numbers[mid]):
+                    return(i+1,mid+1)
+                elif (t1 < numbers[mid]):
+                    right = mid - 1
+                else :
+                    left = mid + 1
+
         return []
