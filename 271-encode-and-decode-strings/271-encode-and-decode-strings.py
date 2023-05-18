676fbdbfa80f1a7ae6379b1ad3977f8f1c37fe1a
Aditya Thakur <adityat354@gmail.com>
2023-05-18 01:36:25 -0400

    Update 271-encode-and-decode-strings.py

--- 271-encode-and-decode-strings/271-encode-and-decode-strings.py
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.py
@@ -1,1 +1,22 @@
+class Solution:
+    """
+    @param: strs: a list of strings
+    @return: encodes a list of strings to a single string.
+    """
+    def encode(self, strs):
+        new_str = ""
+        for st in strs:
+            new_str += st + ":"
+        return new_str
 
+
+
+    """
+    @param: str: A string
+    @return: dcodes a single string to a list of strings
+    """
+    def decode(self, str):
+        # write your code here
+        result = []
+        result = str.split(":")[0:-1]
+        return result
