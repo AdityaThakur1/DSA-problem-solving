50cff38b4c93fee7c834f55d5b01e1c2c4986f92
Aditya Thakur <adityat354@gmail.com>
2023-05-24 17:01:58 -0400

    Time: 79 ms (36.50%), Space: 16.4 MB (8.94%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -8,32 +8,23 @@ class Solution:
         res = ListNode()
         tail = res
         carry = 0
-        while(l1 and l2):
-            s = l1.val + l2.val + carry
+        
+        while(l1 or l2):
+            l1val = l1.val if l1 else 0
+            l2val = l2.val if l2 else 0
+            
+            s = l1val + l2val + carry
             carry = s//10
             s = s%10
             
             tail.next = ListNode(val=s)
             tail = tail.next
             
-            l1, l2 = l1.next, l2.next
-        
-        while(l1):
-            s = l1.val + carry
-            carry = s//10
-            s = s%10
-            tail.next = ListNode(val = s)
-            tail = tail.next
-            l1 = l1.next
-        
-        while(l2):
-            s = l2.val + carry
-            carry = s//10
-            s = s%10
-            tail.next = ListNode(val = s)
-            tail = tail.next
-            l2 = l2.next
-            
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+                  
         if carry > 0:
             tail.next = ListNode(val = carry)
         
